c2bb9677369a5e375b3491377f636e44d92ad05c
C-style-wy <417815802@q.com>
2017-09-12 15:01:56 +0800

    wy

--- wxapp/pages/index/index.js
+++ wxapp/pages/index/index.js
@@ -1,10 +1,13 @@
 //index.js
 //获取应用实例
 var app = getApp()
+var mapContext
 Page({
   data: {
-    latitude: 31.878909,
-    longitude: 117.271788,
+    latitude: 0,
+    longitude: 0,
+    map_width: 0,
+    map_height: 0,
     controls: [{
       id: 1,
       iconPath: '/resources/location.jpg',
@@ -17,54 +20,65 @@ Page({
       clickable: true
     }]
   },
-
   onLoad: function () {
     console.log('onLoad')
+    
+  },
+
+  onReady: function () {
+    // Do something when page ready.
+    this.mapContext = wx.createMapContext('map4select');
+  },
+
+  onShow: function () {
+    console.log("onShow")
+
     var that = this
     // 获取定位，并把位置标示出来
     app.getLocationInfo(function (locationInfo) {
-      console.log('map', locationInfo);
-      var query = wx.createSelectorQuery()
-      var map = query.select("map4select");
-      map.setCenter(locationInfo);
       that.setData({
         longitude: locationInfo.longitude,
-        latitude: locationInfo.latitude})
+        latitude: locationInfo.latitude
+      })
+      that.moveToLocation()
     })
+    
 
     wx.getSystemInfo({
-      success: function(res) {
-        var left = res.screenWidth - 10 -45;
-        var top = res.screenHeight - 20 - 44 - 10 - 45;
-
-        console.log(left);
-        console.log(top);
+      success: function (res) {
+        var left = res.windowWidth - 10 - 45;
+        var top = res.windowHeight - 10 - 45;
 
         that.setData({
           'controls[0].position.left': left,
           'controls[0].position.top': top
         })
       },
-      fail: function(res) {},
-      complete: function(res) {},
+      fail: function (res) { },
+      complete: function (res) { },
     })
   },
 
   controltap: function (e) {
     console.log(e.controlId);
     if (e.controlId == 1) {
-      wx.getLocation({
-        type: 'gcj02', //返回可以用于wx.openLocation的经纬度
-        success: function (res) {
-          var latitude = res.latitude
-          var longitude = res.longitude
-          wx.openLocation({
-            latitude: latitude,
-            longitude: longitude,
-            scale: 28
-          })
-        }
-      })
+      // wx.getLocation({
+      //   type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      //   success: function (res) {
+      //     var latitude = res.latitude
+      //     var longitude = res.longitude
+      //     wx.openLocation({
+      //       latitude: latitude,
+      //       longitude: longitude,
+      //       scale: 28
+      //     })
+      //   }
+      // })
+      this.moveToLocation()
     }
+  },
+
+  moveToLocation: function () {
+    this.mapContext.moveToLocation()
   },
 })
